9cc6057b9fbaadfd387d1660bfb632f80ba94916
Dejan <ddinic2@gmail.com>
2018-12-24 10:17:47 +0100

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { AbscencesListComponent } from './pages/absence-overview/abscences-list/abscences-list.component';
 import { LeaveComponent } from './pages/leave/leave.component';
 import { AuthGuard } from 'src/app/shared/shared/auth.guard';
-import { Motiv8Component } from 'src/app/pages/motiv8/motiv8.component'
+import { Motiv8Component } from 'src/app/pages/motiv8/motiv8.component';
 
 const routes: Routes = [
   { path: 'lista-odsustava',
