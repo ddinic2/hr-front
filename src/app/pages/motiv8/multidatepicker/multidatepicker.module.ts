0f6f80097ffc9434c41d10078e1f199f920e74b3
Dejan <ddinic2@gmail.com>
2018-12-25 11:15:38 +0100

    merge sa bogdanom

--- src/app/pages/motiv8/multidatepicker/multidatepicker.module.ts
+++ src/app/pages/motiv8/multidatepicker/multidatepicker.module.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule, MatButtonModule,
-  MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+  MatDatepickerModule, MatDialogModule,
+  MatFormFieldModule, MatInputModule } from '@angular/material';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MultiDatepickerComponent } from './multidatepicker.component';
 import { YearPickerComponent } from './year-picker-component/year-picker.component';
