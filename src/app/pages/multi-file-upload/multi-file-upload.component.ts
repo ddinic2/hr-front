ee281f19d77579584aed61e7433c0625c4b97191
Dejan Dinic <ddinic2@gmail.com>
2019-04-24 11:30:19 +0200

    multi-file upload component

---
+++ src/app/pages/multi-file-upload/multi-file-upload.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FileUploader, FileLikeObject } from 'ng2-file-upload';
+
+@Component({
+  selector: 'hr-multi-file-upload',
+  templateUrl: './multi-file-upload.component.html',
+  styleUrls: ['./multi-file-upload.component.scss']
+})
+export class MultiFileUploadComponent implements OnInit {
+
+  public uploader: FileUploader = new FileUploader({});
+  public hasBaseDropZoneOver = false;
+
+  constructor() { }
+
+  getFiles(): FileLikeObject[] {
+    return this.uploader.queue.map((fileItem) => {
+      return fileItem.file;
+    });
+  }
+
+  fileOverBase(ev): void {
+    this.hasBaseDropZoneOver = ev;
+  }
+
+  reorderFiles(reorderEvent: CustomEvent): void {
+    const element = this.uploader.queue.splice(reorderEvent.detail.from, 1)[0];
+    this.uploader.queue.splice(reorderEvent.detail.to, 0, element);
+  }
+
+  ngOnInit() {
+  }
+
+}
